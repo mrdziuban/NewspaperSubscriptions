9a63962c1f8dcbecf75aac56a67ff39eecd4488a
mrdziuban <mrdziuban@gmail.com>
2013-07-17 16:47:37 -0700

    Add user and newspaper associations.

--- app/models/newspaper.rb
+++ app/models/newspaper.rb
@@ -1,5 +1,7 @@
 class Newspaper < ActiveRecord::Base
   attr_accessible :editor, :title
 
+  belongs_to :user, foreign_key: :editor
+
   validates :title, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :password, :password_confirmation
   has_secure_password
 
+  has_many :newspapers, foreign_key: :editor
+
   validates :name, presence: true, uniqueness: true
   validates :password, presence: :true, length: {minimum: 6}, on: :create
   validates :password, confirmation: true, allow_nil: true
