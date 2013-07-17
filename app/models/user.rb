dab39b9feb6b68e347cbb02aecaaaa8e19283f1d
mrdziuban <mrdziuban@gmail.com>
2013-07-17 16:27:33 -0700

    Add password validation to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :password, :password_confirmation
+  has_secure_password
 
   validates :name, presence: true, uniqueness: true
+  validates :password, presence: :true, length: {minimum: 6}, on: :create
+  validates :password, confirmation: true, allow_nil: true
+  validates :password_confirmation, presence: true, length: {minimum: 6}, on: :create
 end
